31b3fb03ce9dc9cf8060aaca4902792f4caea153
lantip <lantip@gmail.com>
2020-03-26 13:45:04 +0700

    perbaikan

--- corona-cli.py
+++ corona-cli.py
@@ -37,6 +37,15 @@ def getimage():
                         imej = lis.contents[0].get('src')
                 except:
                     pass
+    if not imej:
+        for lis in lists:
+            if 'download' in lis.get('href'):
+                if len(lis.contents) > 0:
+                    try:
+                        if lis.contents[0].get('src'):
+                            imej = lis.contents[0].get('src')
+                    except:
+                        pass
     return imej
 
 def saveimage(imej):
@@ -49,10 +58,10 @@ def saveimage(imej):
     return 'kemkesimage/'+str(skrg)+'.'+ext
 
 def parseimage(image):
-    img_cv  = cv2.imread(image)
-    #img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)   
-    hasil = pytesseract.image_to_string(img_cv)
-    #data =  pytesseract.image_to_data(Image.open(image))
+    #img_cv  = cv2.imread(image)
+    #img_cv = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)   
+    #hasil = pytesseract.image_to_string(img_cv)
+    hasil =  pytesseract.image_to_string(Image.open(image))
     #print(data)
     try:
         hasil = hasil.split('\n')
@@ -68,7 +77,7 @@ def parseimage(image):
     sembuh = 0
     negatif = 0
     proses = 0
-
+    print(result)
     for idx,rs in enumerate(result):
         if idx > 0:
             if 'indonesia' in result[idx-1].lower():
@@ -91,7 +100,7 @@ def parseimage(image):
     return [diperiksa,positif,sembuh,meninggal,negatif,proses]
 
 def gettable(table):
-    t = PrettyTable(['Diperiksa', 'Positif','Sembuh','Meninggal', 'Negatif', 'Proses'])
+    t = PrettyTable(['Hari ke', 'Penambahan Kasus Hari ini', 'Total Kasus Positif','Sembuh','Meninggal', 'Dalam Perawatan'])
     t.add_row(table)
     return t
 
@@ -102,15 +111,33 @@ def main():
 
     output = args.output
 
-    start = getimage()
+    #start = getimage()
 
-    image = saveimage(start)
+    #image = saveimage(start)
 
-    parse = parseimage(image)
-
+    #parse = parseimage(image)
+    r = requests.get('https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/ArcGIS/rest/services/Statistik_Perkembangan_COVID19_Indonesia/FeatureServer/0/query?where=FID%3E0&objectIds=&time=&resultType=none&outFields=*&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&sqlFormat=none&f=pjson&token=', verify=False)
+    data = r.json()
+    features = data['features']
+    awal = datetime(2020,3,1)
+    skrg = datetime.now()
+    sisa = skrg - awal
+    tabel = []
+    for tbl in features:
+        attrib = tbl['attributes']
+        if attrib['Hari_ke'] == sisa.days:
+            if attrib['Jumlah_Kasus_Baru_per_Hari']:
+                tabel = [ attrib['Hari_ke'], attrib['Jumlah_Kasus_Baru_per_Hari'], attrib['Jumlah_Kasus_Kumulatif'], attrib['Jumlah_Pasien_Sembuh'], attrib['Jumlah_Pasien_Meninggal'], attrib['Jumlah_pasien_dalam_perawatan'] ]
+    if len(tabel) < 1:
+        print('Data hari ini belum diumumkan. Data hari kemarin adalah')
+        for tbl in features:
+            attrib = tbl['attributes']
+            if attrib['Hari_ke'] == (int(sisa.days) - 1):
+                if attrib['Jumlah_Kasus_Baru_per_Hari']:
+                    tabel = [ attrib['Hari_ke'], attrib['Jumlah_Kasus_Baru_per_Hari'], attrib['Jumlah_Kasus_Kumulatif'], attrib['Jumlah_Pasien_Sembuh'], attrib['Jumlah_Pasien_Meninggal'], attrib['Jumlah_pasien_dalam_perawatan'] ]
     if output == 'table':
-        table = gettable(parse)
-        print(table)
+        result = gettable(tabel)
+        print(result)
 
 if __name__ == '__main__':
     main()
